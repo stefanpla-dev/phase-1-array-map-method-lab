891a936ef8b8bb90a0120b11cc9bafcff37f0ddb
Stefan Pla <spla@icloud.com>
2024-02-04 16:59:39 -0500

    complete lab

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+    return tutorials.map(sentence => {
+      const words = sentence.split(" ");
+      const capitalizedWords = words.map(word => {
+        const firstLetter = word.charAt(0).toUpperCase();
+        const restOfWord = word.slice(1);
+        return firstLetter + restOfWord;
+      });
+      return capitalizedWords.join(" ");
+    });
 }
